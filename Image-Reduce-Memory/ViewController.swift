2e0faf10634c77029837db5182202b76a2a49608
Anıl <anilakkaya58@gmail.com>
2021-10-23 17:27:56 +0300

    hello world

--- Image-Reduce-Memory/ViewController.swift
+++ Image-Reduce-Memory/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
+    let url = "https://i.imgur.com/FX1HVet.png"
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    @IBAction func downSampleButtonTapped(_ sender: Any) {
+        let url = URL(string: self.url)!
+        let image = self.downsample(imageURL: url, pointSize: self.imgView.bounds.size)
+        self.imgView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+3.5) {
+            let url = URL(string: self.url)!
+            self.downloadImage(from: url)
+        }
     }
 
-
+    func downloadImage(from url: URL) {
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            // always update the UI from the main thread
+            DispatchQueue.main.async() {
+                self.view.bringSubviewToFront(self.btn)
+                self.imgView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func downsample(imageURL: URL, pointSize: CGSize) -> UIImage? {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions) else {
+            return nil
+        }
+        let scale: CGFloat = UIScreen.main.scale
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+        ] as CFDictionary
+        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
+            return nil
+        }
+        return UIImage(cgImage: downsampledImage)
+    }
 }
-
